d732321b2c63993d797055ca7a313efd8b4c8c85
user <jihn2824@gmail.com>
2023-10-23 11:56:06 +0900

    crawling_data

--- 01_crawling_data.py
+++ 01_crawling_data.py
@@ -8,53 +8,53 @@ import re
 import time
 import datetime
 
-
 options = ChromeOptions()
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
 options.add_argument('user-agent=' + user_agent)
 options.add_argument('lang=ko_KR')
-
-
 
 # 크롬 드라이버 최신 버전 설정
 service = ChromeService(executble_path=ChromeDriverManager().install())
 # 크롬 드라이버
 driver = webdriver.Chrome(service=service, options=options)
 
-url = 'https://movie.daum.net/'
 for year in range(18, 21):
-    year_url = url + 'ranking/boxoffice/monthly?date=20{}'.format(year)
-    url = year_url
+    url = 'https://movie.daum.net/ranking/boxoffice/monthly?date=20{}'.format(year)
     df_title = pd.DataFrame()
-    title = []
-    review = []
+
+
     for month in range(1, 13):
         month_url = url + '{}'.format(month).zfill(2)
         url = month_url
         driver.get(url)
         time.sleep(3)
         for movie in range (1, 31):
-            movie_data = '//*[@id="mainContent"]/div/div[2]/ol/li[{}]/div/div[2]/strong/a'.format(movie)
-            driver.find_element(By.XPATH, movie_data).click()
-            driver.get(url)
-            movie_data = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', movie_data)
-            title.append(movie_data)
-            time.sleep(3)
-            print('count = %d' % movie)
+            movie_data = driver.find_element(By.XPATH,'//*[@id="mainContent"]/div/div[2]/ol/li[{}]/div/div[2]/strong/a'.format(movie))
+            title = movie_data.text
+            titles = []
+            reviews = []
+            if title not in titles :
+                title = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', title)
+                titles.append(movie_data)
+                movie_data.click()
+                time.sleep(3)
+                print(title)
 
-            review_tap = '//*[@id="mainContent"]/div/div[2]/div[1]/ul/li[4]/a/span'
-            driver.find_element('xpath', review_tap).click()
-            for more in range (5):
-                see_more = '//*[@id="alex-area"]/div/div/div/div[3]/div[1]/button'.format(more)
-                driver.find_element('xpath', see_more).click()
-            try :
-                for review in range (1, 161) :
-                    review_data = driver.find_element(
-                        'xpath','/html/body/div[2]/main/article/div/div[2]/div[2]/div/div/div[2]/div/div/div/div[3]/ul[2]/li[{}]/div/p'.format(review)).text
-                    review_data = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', review_data)
-                    title.append(review_data)
-            except : pass
+                review_tap = driver.find_element(By.XPATH,'//*[@id="mainContent"]/div/div[2]/div[1]/ul/li[4]/a/span')
+                review_tap.click()
+                time.sleep(1)
+                for more in range (5):
+                    see_more = driver.find_element(By.XPATH,'//*[@id="alex-area"]/div/div/div/div[3]/div[1]/button'.format(more))
+                    see_more.click()
+                    time.sleep(1)
+                    try :
+                        for review in range (1, 161) :
+                            review_data = driver.find_element(
+                                'xpath','/html/body/div[2]/main/article/div/div[2]/div[2]/div/div/div[2]/div/div/div/div[3]/ul[2]/li[{}]/div/p'.format(review)).text
+                            review_data = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', review_data)
+                            reviews.append(review_data)
+                    except : pass
 
-            df_section_title = pd.DataFrame(title, columns=['title'])
-            df_section_title['review'] = review
-            df_titles = pd.concat([df_title, df_section_title], ignore_index=True)
+                df_section_title = pd.DataFrame(titles, columns=['title'])
+                df_section_title['review'] = reviews
+                df_titles = pd.concat([df_title, df_section_title], ignore_index=True)
