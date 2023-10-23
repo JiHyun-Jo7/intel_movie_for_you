5601e868b99f5d4249a08e297a9a55eb72e8dca0
Yoon Hee Seung <iyhs1858@gmail.com>
2023-10-23 11:29:28 +0900

    Job1 : Crawling Movie Reviews - YHS

--- 01_crawling_data.py
+++ 01_crawling_data.py
@@ -1,60 +1,78 @@
-from selenium import webdriver              # 웹사이트(및 웹 애플리케이션)의 유효성 검사에 사용되는 자동화 테스트 프레임워크
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 import pandas as pd
 import re
 import time
 import datetime
 
-
 options = ChromeOptions()
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
-options.add_argument('user-agent=' + user_agent)
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.57'
+options.add_argument('user-agent='+user_agent)
 options.add_argument('lang=ko_KR')
+# options.add_argument('window-size=1920x1080')
+# options.add_argument('disable-gpu')
+# options.add_argument('--no-sandbox')
 
-
-
-# 크롬 드라이버 최신 버전 설정
-service = ChromeService(executble_path=ChromeDriverManager().install())
-# 크롬 드라이버
+service = ChromeService(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
-url = 'https://movie.daum.net/'
-for year in range(18, 21):
-    year_url = url + 'ranking/boxoffice/monthly?date=20{}'.format(year)
-    url = year_url
-    df_title = pd.DataFrame()
-    title = []
-    review = []
-    for month in range(1, 13):
-        month_url = url + '{}'.format(month).zfill(2)
-        url = month_url
+years = [2021, 2022, 2023]
+url = 'https://movie.daum.net/ranking/boxoffice/monthly?date='
+titles = []
+
+for year in years:
+    for month in range(1,13):
+        df_movies = pd.DataFrame()
+
+        date = str(year)+str('%02d'%month)
+        url = url+date
         driver.get(url)
-        time.sleep(3)
-        for movie in range (1, 31):
-            movie_data = '//*[@id="mainContent"]/div/div[2]/ol/li[{}]/div/div[2]/strong/a'.format(movie)
-            driver.find_element(By.XPATH, movie_data).click()
-            driver.get(url)
-            movie_data = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', movie_data)
-            title.append(movie_data)
-            time.sleep(3)
-            print('count = %d' % movie)
+        time.sleep(2)
 
-            review_tap = '//*[@id="mainContent"]/div/div[2]/div[1]/ul/li[4]/a/span'
-            driver.find_element('xpath', review_tap).click()
-            for more in range (5):
-                see_more = '//*[@id="alex-area"]/div/div/div/div[3]/div[1]/button'.format(more)
-                driver.find_element('xpath', see_more).click()
-            try :
-                for review in range (1, 161) :
-                    review_data = driver.find_element(
-                        'xpath','/html/body/div[2]/main/article/div/div[2]/div[2]/div/div/div[2]/div/div/div/div[3]/ul[2]/li[{}]/div/p'.format(review)).text
-                    review_data = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', review_data)
-                    title.append(review_data)
-            except : pass
+        reviews = []
+        title = ''
+        for movie in range(1, 31):
+            movie_title = driver.find_element('xpath','//*[@id="mainContent"]/div/div[2]/ol/li[{}]/div/div[2]/strong/a'.format(movie))
+            title = movie_title.text
+            if title not in titles:
+                titles.append(title)
+                movie_title.click()
+                time.sleep(2)
+                movie_review = driver.find_element('xpath','//*[@id="mainContent"]/div/div[2]/div[1]/ul/li[4]/a/span').click()
+                time.sleep(2)
 
-            df_section_title = pd.DataFrame(title, columns=['title'])
-            df_section_title['review'] = review
-            df_titles = pd.concat([df_title, df_section_title], ignore_index=True)
+                button_cnt = driver.find_element('xpath','//*[@id="mainContent"]/div/div[2]/div[2]/div/strong/span').text
+                button_cnt = re.sub(r'[^0-9]','',button_cnt)
+                button_cnt = 1+int(button_cnt)//30 if 1+int(button_cnt)//30 < 5 else 5 # 총 리뷰 개수를 찾아서 리뷰 더보기 누를 횟수 계산
+
+                for i in range(button_cnt):
+                    review_button = driver.find_element('xpath','//*[@id="alex-area"]/div/div/div/div[3]/div[1]/button').click()
+                    time.sleep(1)
+                for r in range(1,161):
+                    try:
+                        review = driver.find_element('xpath','/html/body/div[2]/main/article/div/div[2]/div[2]/div/div/div[2]/div/div/div/div[3]/ul[2]/li[{}]/div/p'.format(r)).text
+                        review = re.compile('[^가-힣]').sub(' ',review)
+                        reviews.append(review)
+                    except:
+                        print('Error {}{} movie {} {}th review'.format(year,month, movie, r))
+            if movie % 5 == 0:
+                df_movie_review = pd.DataFrame(reviews, columns=['review'])
+                df_movie_review['title'] = title
+                df_movies = pd.concat([df_movies, df_movie_review], ignore_index=True)
+                df_movies = df_movies.reindex(['title','review'],axis=1)
+                df_movies.to_csv('./crawling_data/movie_reviews_{}{}_{}.csv'.format(year,month,movie),index=False)
+                reviews = []
+
+            driver.back()
+            time.sleep(2)
+            driver.back()
+            time.sleep(2)
+
+        df_movie_review = pd.DataFrame(reviews,columns=['review'])
+        df_movie_review['title'] = title
+        df_movies = pd.concat([df_movies, df_movie_review], ignore_index=True)
+        df_movies = df_movies.reindex(['title', 'review'], axis=1)
+        df_movies.to_csv('./crawling_data/movie_reviews_{}{}_last.csv'.format(year, month), index=False)
