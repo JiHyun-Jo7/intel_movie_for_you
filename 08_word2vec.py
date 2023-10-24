5a1c8cee4e3e0fb55c4ba562680d36d66d485348
user <jihn2824@gmail.com>
2023-10-24 16:35:28 +0900

    Word_2_Vec

--- 08_word2vec.py
+++ 08_word2vec.py
@@ -15,3 +15,6 @@ print(tokens[0])
 
 # 100차원, 4개씩 끊어서, 단어 최소 사용 횟수 20회 이상, core 사용 수 = 8, 학습 100회,
 embedding_model = Word2Vec(tokens, vector_size=100, window=4, min_count=20, workers=8, epochs=100, sg=1)
+embedding_model.save('./models/word2vec_movie_review.model')
+print(list(embedding_model.wv.index_to_key))
+print(len(embedding_model.wv.index_to_key))
