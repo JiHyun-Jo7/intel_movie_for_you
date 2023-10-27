dd611c897b5b3084cb0e5f8ea54a8f010d2a8271
Younghoda <dudgh597@naver.com>
2023-10-27 11:43:49 +0900

    movie_recommendation

---
+++ movie_recommendation_app.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+form_window = uic.loadUiType('./cat_and_dog/cats_and_dogs.ui')[0]
+class Exam(QWidget, form_window):           # 클래스 생성
+    def __init__(self):
+        super().__init__()                  # 부모 클래스
+        self.setupUi(self)
+
+
+if __name__ == '__main__':                  # 메인
+    app = QApplication(sys.argv)            # 객체 생성
+    MainWindow = Exam()
+    MainWindow.show()                       # 창를 화면에 출력
+    sys.exit(app.exec_())                   # 창 화면 출력을 유지하는 함수
