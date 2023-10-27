01f0fe05e8bfca8f26da15dcfb645d29c24547c5
Younghoda <dudgh597@naver.com>
2023-10-27 09:47:34 +0900

    Movie_Recommendation

--- 07_movie_recommendation.py
+++ 07_movie_recommendation.py
@@ -4,6 +4,7 @@ from scipy.io import mmread
 import pickle
 from konlpy.tag import Okt
 import re
+from gensim.models import Word2Vec
 
 def getReccomendation(cosine_sim):
     simScore = list(enumerate(cosine_sim[-1]))
@@ -19,9 +20,12 @@ Tfidf_matrix = mmread('./models/Tfidf_movie_review.mtx').tocsr()
 with open('./models/tfidf.pickle', 'rb') as f:
     Tfidf = pickle.load(f)
 
-print(df_reviews.iloc[0, 0])
-cosine_sim = linear_kernel(Tfidf_matrix[0], Tfidf_matrix)         # 0번째 영화와 전체 영화의 cosine 유사도
-print(cosine_sim[0])
-print(len(cosine_sim[0]))
-recommendation = getReccomendation(cosine_sim)
-print(recommendation)
+# print(df_reviews.iloc[0, 0])
+# cosine_sim = linear_kernel(Tfidf_matrix[0], Tfidf_matrix)         # 0번째 영화와 전체 영화의 cosine 유사도
+# print(cosine_sim[0])
+# print(len(cosine_sim[0]))
+# recommendation = getReccomendation(cosine_sim)
+# print(recommendation)
+
+# keyword 기반 영화 추천
+embedding_model = Word2Vec.load('./models/word2vec_movie_review.model')
